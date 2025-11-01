21a957e0ec35ec380545e3f3d6bbdc7b49d51a9e
pseudo-sue <ofamily94@naver.com>
2025-11-01 20:55:14 +0900

    Update train_loop.py

--- detectron2/engine/train_loop.py
+++ detectron2/engine/train_loop.py
@@ -482,7 +482,7 @@ class AMPTrainer(SimpleTrainer):
         """
         assert self.model.training, "[AMPTrainer] model was changed to eval mode!"
         assert torch.cuda.is_available(), "[AMPTrainer] CUDA is required for AMP training!"
-        from torch.cuda.amp import autocast
+        from torch import autocast
 
         start = time.perf_counter()
         data = next(self._data_loader_iter)
@@ -490,7 +490,7 @@ class AMPTrainer(SimpleTrainer):
 
         if self.zero_grad_before_forward:
             self.optimizer.zero_grad()
-        with autocast(dtype=self.precision):
+        with autocast(device_type='cuda', dtype=self.precision):
             loss_dict = self.model(data)
             if isinstance(loss_dict, torch.Tensor):
                 losses = loss_dict
